154ed1845e09bc41cc3e7facd4daea875dcb5036
phuctran <thienphucst92@gmail.com>
2017-06-25 22:51:22 +0700

    fix bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_action :require_login, only: [:inbox]
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
